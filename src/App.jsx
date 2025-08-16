96cf50d5c5c6d4960328e7ce2102bbc6f09335d6
Krantikumar Mathapati <krantikumar3446@gmail.com>
2025-08-16 12:42:16 +0530

    Team section completed.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
 import Services from "./components/Services";
 import OurWork from "./components/OurWork";
+import Teams from "./components/Teams";
 
 function App() {
   const [theme, setTheme] = useState(
@@ -17,6 +18,7 @@ function App() {
       <TrustedBy/>
       <Services/>
       <OurWork/>
+      <Teams/>
     </div>
   );
 }
