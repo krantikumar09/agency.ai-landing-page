49ac1151b499de6dbf13ffb57f328338c457409b
Krantikumar Mathapati <krantikumar3446@gmail.com>
2025-08-16 12:35:23 +0530

    Our Work section completed.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
 import Services from "./components/Services";
+import OurWork from "./components/OurWork";
 
 function App() {
   const [theme, setTheme] = useState(
@@ -15,6 +16,7 @@ function App() {
       <Hero />
       <TrustedBy/>
       <Services/>
+      <OurWork/>
     </div>
   );
 }
