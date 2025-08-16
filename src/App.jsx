41514d3ba228536c0125d32065add549c82f0448
Krantikumar Mathapati <krantikumar3446@gmail.com>
2025-08-16 11:19:13 +0530

    Navbar completed.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+
+  return (
+    <div className="dark:bg-black relative">
+      <Navbar theme={theme} setTheme={setTheme} />
+    </div>
+  );
+}
+
+export default App;
